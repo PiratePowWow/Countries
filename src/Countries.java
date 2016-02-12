87ed217b88b7eaf0da82821c0e586fbacde93713
PiratePowWow <Thi3v3ry>
2016-02-12 17:12:39 -0500

    2.4 Early Save

--- src/Countries.java
+++ src/Countries.java
@@ -1,10 +1,51 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Countries {
-    public static void main(String[] args) {
+    public static HashMap<Character, ArrayList<String>> countries = new HashMap<>();
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter a letter:");
+
+            String fileName = scanner.nextLine() + "_Countries";
+            String
+            writeFile(fileName, fileContent);
+
+        }
+
+
+
+    }
+
+    static String fileContent(String fileName){
+
+
+    }
+
+    static void readFile(String fileName) throws FileNotFoundException {
+        File f = new File(fileName);
+        Scanner fileScanner = new Scanner(f);
+        while (fileScanner.hasNext()) {
+            String line = fileScanner.nextLine();
+            String[] columns = line.split("\\|");
+            Country country = new Country();
+            country.setAbbreviation(columns[0]);
+            country.setCountry(columns[1]);
+            if (countries.containsKey(columns[1].charAt(0))){
+                countries.get(columns[1].charAt(0)).add(columns[1]);
+            }
+            else {
+                countries.put(columns[1].charAt(0), new ArrayList<String>());
+                countries.get(columns[1].charAt(0)).add(columns[1]);
+            }
+        }
     }
 
     static void writeFile(String fileName, String fileContent) throws IOException {
---
+++ src/Country.java
@@ -0,0 +1,24 @@
+/**
+ * Created by PiratePowWow on 2/12/16.
+ */
+public class Country {
+    private String abbreviation;
+    private String country;
+
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public void setAbbreviation(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
